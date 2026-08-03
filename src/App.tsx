6850ee829dbd2290b09e0bdc77f4e5006d662c3c
Marcia Silva <contato.pix@gmail.com>
2026-08-03 14:57:50 -0300

    Marcação HTML dos botões de filtro

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,16 @@
 function App() {
 
   return (
-    <>
-      <h1>Hello world!</h1>
-    </>
+    <div className="container">
+      <h1 className="text-center my-8">Lista de tarefas</h1>
+
+      <div className="md:flex md:align-items md:justify-center gap-3 rounded-lg bg-white border border-zinc-200 p-4">
+        <button className="btn border-zinc-400 text-zinc-500 hover:bg-zinc-500/20">Todas</button>
+        <button className="btn border-red-300 text-red-500 hover:bg-red-500/20">Pendentes</button>
+        <button className="btn border-green-300 text-green-500 hover:bg-green-500/20">Concluídas</button>
+      </div>
+
+    </div>
   )
 }
 
